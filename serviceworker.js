0aece9493fe7016e6dfff6adec7db28d5316b570
NMFrazi <Nidra.Frazier@gmail.com>
2020-07-13 00:14:01 -0500

    inital commit

---
+++ serviceworker.js
@@ -0,0 +1,30 @@
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
+);
+
+importScripts(
+  "/clicky-game/precache-manifest.ff65360cc28956ec3c5d5c383b0ad29b.js"
+);
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(
+  workbox.precaching.getCacheKeyForURL("/clicky-game/index.html"),
+  {
+    blacklist: [/^\/_/, /\/[^/?]+\.[^/]+$/],
+  }
+);
